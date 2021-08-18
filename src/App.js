f1e0a53036afa48c463eea8841da619406c7f4c0
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-08-19 00:45:49 +0300

    задание 1 и 2 без стилей

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Section from './components/Section';
+import ProfileList from './components/Profile/ProfileList';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
 
 function App() {
   return (
     <div>
-      <Section title="Задание 1 - Профиль социальной сети"></Section>
-      <Section title="Задание 2 - Секция статистики"></Section>
+      <Section title="Задание 1 - Профиль социальной сети">
+        <ProfileList />
+      </Section>
+      <Section title="Задание 2 - Секция статистики">
+        <Statistics title="Upload stats" stats={statisticalData} />
+      </Section>
       <Section title="Задание 3 - Список друзей"></Section>
       <Section title="Задание 4 - История транзакций"></Section>
     </div>
